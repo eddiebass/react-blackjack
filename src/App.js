adcdbab76d8b3324e0ba35754b4bb0b0a23c912d
Eddie Bassey <eddie.bassey@nology.io>
2022-05-16 23:12:39 +0100

    add card page add connect to card api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Cards from "./components/Cards";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [deck, setDeck] = useState({});
+	function getCards() {
+		fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+			.then((res) => res.json())
+			.then((data) => {
+				setDeck(data);
+			});
+	}
+	console.log(deck);
+	return (
+		<div className="App">
+			<h1>BLACKJACK</h1>
+			<Cards />
+			<p>Draw a Card</p>
+			<h2>Sum: 0</h2>
+			<div>
+				<button onClick={getCards}>Start A Game </button>
+				<button className="blue">Draw A Game </button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Cards.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Cards = () => {
+	return (
+		<section>
+			<div className="card">Card 1</div>
+			<div className="card">Card 2</div>
+		</section>
+	);
+};
+
+export default Cards;
